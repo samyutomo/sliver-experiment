81c83ebe8f187f33cd3580662f190f853b1b1ebe
samyutomo <samyutomo@gmail.com>
2022-07-01 19:09:32 +0700

    starter code of forecasting app called Horizons. This MaterialApp consists of a Scaffold with a WeeklyForecastList as the body. This widget consists of a SingleChildScrollView that contains a Column featuring the next 7-day forecast.

---
+++ android/app/src/main/kotlin/id/samyutomo/app/sliver/MainActivity.kt
@@ -0,0 +1,6 @@
+package id.samyutomo.app.sliver
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SliverApp());
+}
+
+class SliverApp extends StatelessWidget {
+  const SliverApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      scrollBehavior: const ConstantScrollBehavior(),
+      title: "Horizon Weather",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Horizons"),
+          backgroundColor: Colors.teal[800],
+        ),
+        body: const WeeklyForecastList(),
+      ),
+    );
+  }
+
+}
+
+class WeeklyForecastList extends StatelessWidget {
+  const WeeklyForecastList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime currentDate = DateTime.now();
+    TextTheme textTheme = Theme
+        .of(context)
+        .textTheme;
+    List<DailyForecast> forecasts = Server.getDailyForecastList();
+
+    return SingleChildScrollView(
+      child: Column(
+        children: forecasts.map((dailyForecast) {
+          return Card(
+            child: ListTile(
+              leading: Text(
+                dailyForecast.getDate(currentDate.day).toString(),
+                style: textTheme.headline4,
+              ),
+              title: Text(
+                dailyForecast.getWeekday(currentDate.weekday),
+                style: textTheme.headline5,
+              ),
+              subtitle: Text(dailyForecast.description),
+              trailing: Text(
+                '${dailyForecast.highTemp} | ${dailyForecast.lowTemp} F',
+                style: textTheme.subtitle2,
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+}
+
+const String baseAssetURL = 'https://dartpad-workshops-io2021.web.app/getting_started_with_slivers/';
+const String headerImage = "${baseAssetURL}assets/header.jpeg";
+
+const Map<int, DailyForecast> _kDummyData = {
+  0: DailyForecast(
+    id: 0,
+    imageId: '${baseAssetURL}assets/day_0.jpeg',
+    highTemp: 73,
+    lowTemp: 52,
+    description:
+    'Partly cloudy in the morning, with sun appearing in the afternoon.',
+  ),
+  1: DailyForecast(
+    id: 1,
+    imageId: '${baseAssetURL}assets/day_1.jpeg',
+    highTemp: 70,
+    lowTemp: 50,
+    description: 'Partly sunny.',
+  ),
+  2: DailyForecast(
+    id: 2,
+    imageId: '${baseAssetURL}assets/day_2.jpeg',
+    highTemp: 71,
+    lowTemp: 55,
+    description: 'Party cloudy.',
+  ),
+  3: DailyForecast(
+    id: 3,
+    imageId: '${baseAssetURL}assets/day_3.jpeg',
+    highTemp: 74,
+    lowTemp: 60,
+    description: 'Thunderstorms in the evening.',
+  ),
+  4: DailyForecast(
+    id: 4,
+    imageId: '${baseAssetURL}assets/day_4.jpeg',
+    highTemp: 67,
+    lowTemp: 60,
+    description: 'Severe thunderstorm warning.',
+  ),
+  5: DailyForecast(
+    id: 5,
+    imageId: '${baseAssetURL}assets/day_5.jpeg',
+    highTemp: 73,
+    lowTemp: 57,
+    description: 'Cloudy with showers in the morning.',
+  ),
+  6: DailyForecast(
+    id: 6,
+    imageId: '${baseAssetURL}assets/day_6.jpeg',
+    highTemp: 75,
+    lowTemp: 58,
+    description: 'Sun throughout the day.',
+  ),
+};
+
+class Server {
+  static List<DailyForecast> getDailyForecastList() => _kDummyData.values.toList();
+
+  static DailyForecast getDailyForecastByID(int id){
+    assert(id >= 0 && id <= 6);
+    return _kDummyData[id]!;
+  }
+}
+
+class DailyForecast {
+  const DailyForecast({
+    required this.id,
+    required this.imageId,
+    required this.highTemp,
+    required this.lowTemp,
+    required this.description
+  });
+
+  final int id;
+  final String imageId;
+  final int highTemp;
+  final int lowTemp;
+  final String description;
+
+  static const List<String> _weekdays = <String>[
+    "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"
+  ];
+
+  String getWeekday(int today){
+    final int offset = today + id;
+    final int day = offset >= 7? offset - 7 : offset;
+    return _weekdays[day];
+  }
+
+  int getDate(int today) => today + id;
+
+
+}
+
+class ConstantScrollBehavior extends ScrollBehavior {
+  const ConstantScrollBehavior();
+
+  @override
+  Widget buildScrollbar(BuildContext context, Widget child,
+      ScrollableDetails details) {
+    return child;
+  }
+
+  @override
+  Widget buildOverscrollIndicator(BuildContext context, Widget child,
+      ScrollableDetails details) {
+    return child;
+  }
+
+  @override
+  TargetPlatform getPlatform(BuildContext context) {
+    return TargetPlatform.macOS;
+  }
+
+  @override
+  ScrollPhysics getScrollPhysics(BuildContext context) {
+    return const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics());
+  }
+}
